10341711f84897644c3a41143c9a9fbf7feab107
Matheus Ferreira <200024949@aluno.unb.br>
2023-01-11 10:09:11 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "oi");
-		System.out.println(obj);
+		Seller seller = new Seller(19,"Mat", "mm",new Date(),3000, obj );
+		System.out.println(seller);
 
 	}
 
